7ee606f2845b360fe62a93237a83448f19008b4b
[Christopher Andrew Topalian] <[scriptingdata@gmail.com]>
2025-03-23 10:22:28 -0400

    update

--- src/js/4make/makeTitleOfApp.js
+++ src/js/4make/makeTitleOfApp.js
@@ -14,7 +14,7 @@ function makeTitleOfApp()
     let titleOfApp = ce('a');
     titleOfApp.id = 'titleOfApp';
     titleOfApp.textContent = 'CATopalian JavaScript List Generator';
-    titleOfApp.href = 'https://github.com/christopherandrewtopalian/';
+    titleOfApp.href = 'https://github.com/ChristopherAndrewTopalian/CATopalian_JavaScript_List_Generator';
     titleOfApp.target = '_blank';
     titleOfApp.style.fontSize = '17px';
     titleOfApp.style.fontWeight = 'bold';
